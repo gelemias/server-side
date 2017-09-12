c29d21bec4e14a0cefa1005e489297d525032776
Guillermo Delgado <gelemias@gmail.com>
2017-09-12 09:01:43 +0200

    Templating with Leaf

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "HelloWorld",
+    name: "server-side",
     targets: [
         Target(name: "App"),
         Target(name: "Run", dependencies: ["App"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -33,6 +33,30 @@ extension Droplet {
             return try JSON(node: ["message": "hello pinkfloid \(name)"])
         }
 
+        get("template1") { req in
+            guard let name = req.data["name"]?.string else {
+                return "⚠️ Please provide a valid name as ?name=<your_name>"
+            }
+
+            return try self.view.make("hello", Node(node:["name": name]))
+        }
+
+        get("template2") { req in
+            let users = try [["name": "Ray", "email": "ray@tutorials.com"],
+                             ["name": "Ignatius", "email": "ignatius@farray.com"],
+                             ["name": "David", "email": "david@broncano.com"],
+                             ["name": "Queque", "email": "queque@demiguel.com" ]].makeNode(in: nil)
+            return try self.view.make("hello2", Node(node:["users": users]))
+        }
+
+        get("template3") { req in
+            guard let sayHello = req.data["sayHello"]?.bool else {
+                fatalError()
+            }
+
+            return try self.view.make("hello3", Node(node: ["sayHello": sayHello.makeNode(in: nil)]))
+        }
+
         try resource("posts", PostController.self)
     }
 }
